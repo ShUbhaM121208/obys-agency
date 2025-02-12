04f5a69ed39ce7a9e84b00b2e458da887ddd0c10
Shubham <shubhamm.0010@gmail.com>
2025-02-13 02:17:25 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-var tl = gsap.timeline()
-tl.from(".line h1",{
+function loadingAnimation(){
+    var tl = gsap.timeline()
+    tl.from(".line h1",{
     y :150,
     stagger:.25,
     Duration:.6,
     delay:0.5
-})
-tl.from("#line-part1",{
+   })
+   tl.from("#line-part1",{
     opacity:0,
     onStart : function(){
         var h5timer = document.querySelector("#line-part1 h5")
@@ -45,3 +45,25 @@ tl.from("#page1",{
 tl.to("#loader",{
     display: "none"
 })
+tl.to("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+
+
+}
+loadingAnimation()
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4", {
+      });
+
+}
+cursorAnimation();
